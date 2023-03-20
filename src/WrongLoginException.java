7476c56b4000b8c1c09365e149e9d394efc88ea9
siemens <sema.perstnev.01@mail.ru>
2023-03-20 21:02:22 +0300

    Exceptions. Перстнев С.

---
+++ src/WrongLoginException.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class WrongLoginException extends IOException {
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
---
+++ src/WrongPasswordException.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class WrongPasswordException extends IOException {
+    public WrongPasswordException() {}
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
